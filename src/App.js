346a06aadb11fec76adcb5c94b4f24c840f9c6d8
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2023-12-14 00:41:27 +0530

    add base styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import Clients from "./components/clients";
+import Footer from "./components/footer";
+import Free from "./components/free";
+import Home from "./components/home";
+import Like from "./components/like";
+import Navbar from "./components/navbar";
+import Release from "./components/release";
+import ScrollToTop from "./components/scroll-to-top";
+import SignUp from "./components/sign-up";
+import SuperRare from "./components/superrare";
+
+import "./scss/index.scss";
+
 function App() {
-  return <div>App test</div>;
+  return (
+    <div className="app-container">
+      <ScrollToTop />
+      <Navbar />
+      <Home />
+      <Free />
+      <Clients />
+      <SuperRare />
+      <Release />
+      <Like />
+      <SignUp />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
