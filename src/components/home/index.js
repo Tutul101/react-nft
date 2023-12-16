2aae97743f3c14e98eb42ff66bda402cfdab1d0c
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2023-12-16 17:08:51 +0530

    add home component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,34 @@
 import React from "react";
+import home from "../../assets/home.png";
+
+import "./home.scss";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="container">
+        <div className="content">
+          <p className="sub-title">Launching soon</p>
+          <h1 className="title">An NFT like no other</h1>
+          <p className="description">
+            Don't miss out on the release of our new NFT. Sign up below to
+            recieve updates when we go live.
+          </p>
+          <button className="sign-up">Sign Up</button>
+        </div>
+
+        <div className="image-container">
+          <div className="image">
+            <img src={home} alt="home"></img>
+          </div>
+          {/* <div className="ellipse-container">
+            <div className="ellipse pink"></div>
+            <div className="ellipse orange"></div>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
