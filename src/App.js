3c56b093224be5693866790ea18905bdbc23b497
Tutul101 <60464726+Tutul101@users.noreply.github.com>
2024-01-04 20:17:13 +0530

    add animations using scroll reveal

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import ScrollReveal from "scrollreveal";
 import Clients from "./components/clients";
 import Footer from "./components/footer";
 import Free from "./components/free";
@@ -13,6 +14,27 @@ import SuperRare from "./components/superrare";
 import "./scss/index.scss";
 
 function App() {
+  useEffect(() => {
+    window.setTimeout(() => {
+      const home = document.getElementsByClassName("home");
+      home[0].style.transform = "none";
+      const nav = document.getElementsByClassName("nav-wrapper");
+      nav[0].style.transform = "none";
+    }, 1500);
+    const registerAnimation = () => {
+      const sr = ScrollReveal({
+        origin: "bottom",
+        distance: "80px",
+        duration: 2000,
+        reset: false,
+      });
+      sr.reveal(
+        `.nav,.home,.free,.clients,.super-rare,.relases,.like,.signup,footer`,
+        { interval: 500 }
+      );
+    };
+    registerAnimation();
+  }, []);
   return (
     <div className="app-container">
       <ScrollToTop />
